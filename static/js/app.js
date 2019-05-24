3e5b1f448886683499f922e7c1cc276a76c5ee54
Lei <koudash@gmail.com>
2019-05-24 15:39:16 -0500

    fix the double-click issue for dropdown menu in form

--- static/js/app.js
+++ static/js/app.js
@@ -11,89 +11,87 @@ var basicOptTag = 'opt-filter-tag-';
 var basicOptValue = 'opt-filter-value-';
 var basicBtnDel = 'btn-filter-del-';
 
-// Locate ul tags
-var ulTag = d3.select('#ul-filter-tag');
-var ulValueTag = d3.select('#ul-filter-value');
-var ulDel = d3.select('#ul-filter-del');
+// Locate 'ul' elements
+var ulTagEle = d3.select('#ul-filter-tag');
+var ulValueEle = d3.select('#ul-filter-value');
+var ulDelEle = d3.select('#ul-filter-del');
 
-// Locate buttons
-var addFilter = d3.select('#filter-add');
-var delFilter = d3.select('#filter-del');
-var submit = d3.select('#filter-submit');
+// Locate 'button' element for "Add Filter"
+var butAddEle = d3.select('#filter-add');
 
 // Number of filters applied on dataset, default value equals to 1
 var maxLayer = 1;
 
-// Object to store applied filter names and layers, empty by default
-var usedFilterInfo = {};
-
-// Retrieve array containing filter layer info
-var currAppliedLayer = Object.keys(usedFilterInfo);
+// Object to store applied filter names and layers
+var usedFilterInfo = {'alligator': []};
 
 // UFO dataset
 const ufoDataset = data;
 // Filters used for filtering the UFO dataset
 const ufoFilterArr = ['datetime', 'city', 'state', 'country', 'shape'];
+// names used for different filter layers in html form area
+const filterLayerArr = ['alligator', 'bison', 'cougar', 'coyote', 'raccoon'];
 // Keys of UFO data
 const tableCol = ['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes', 'comments'];
 
-// Event listeners for the forms
+// Event listeners for dropdown menu in the forms
 formDisplayAvailFilters();
 formSelectFilterName();
 formSelectFilterValue();
 
-// >>>>>>>>>>>>>>> EVENT LISTENER 4 >>>>>>>>>>>>>>>
-// >>>>>>>>>>>>>>>       START      >>>>>>>>>>>>>>>
-
-// Event listener for 'Add Filter' button
-addFilter.on("click", function() {
+// >>>>>>>>>>>>>>>    EVENT LISTENER    >>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>  BUTTON: ADD FILTER  >>>>>>>>>>>>>>>
+butAddEle.on("click", function() {
 
     // Prevent the webpage from self-refreshing
     d3.event.preventDefault();
 
-    // Add 1 to number of applied filters
-    maxLayer += 1;
+    // Retrieve array containing filter layer info
+    let currApplLayerArr = Object.keys(usedFilterInfo);
+    // Determine array of currently available filter layer names
+    // Note that since "Add Filter" button is active, there should be layer name(s) available
+    let currAvailLayerArr = filterLayerArr.filter((layer) => !currApplLayerArr.includes(layer));  
+    // Assign the first name of the above array as new layer name and update object of applied filter info
+    let newLayer = currAvailLayerArr.shift();
+    usedFilterInfo[newLayer] = [];    
 
-    // Generate 'id' for new 'li' and 'select' in both forms
-    let newLiFilterId = basicLiTag + maxLayer.toString();
-    let newLiFilterValueId = basicLiValue + maxLayer.toString();
-    let newLiFilterDelId = basicLiDel + maxLayer.toString();
-    let newSelFilterId = basicSelTag + maxLayer.toString();
-    let newSelFilterValueId = basicSelValue + maxLayer.toString();
-    let newBtnFilterDelId = basicBtnDel + maxLayer.toString();
+    // Generate 'id' for new 'li' and 'select' in all forms
+    let newLiTagId = basicLiTag + newLayer;
+    let newLiValueId = basicLiValue + newLayer;
+    let newLiDelId = basicLiDel + newLayer;
+    let newSelTagId = basicSelTag + newLayer;
+    let newSelValueId = basicSelValue + newLayer;
+    let newBtnDelId = basicBtnDel + newLayer;
 
-    // Append new 'li' element to 'ulTag', 'ulValueTag', and 'ulDel' with 'class' and 'id', respectively
-    let newLiTag = ulTag.append('li').attr('class', 'list-group-item').attr('id', `${newLiFilterId}`);
-    let newLiValueTag = ulValueTag.append('li').attr('class', 'list-group-item').attr('id', `${newLiFilterValueId}`);
-    let newLiDel = ulDel.append('li').attr('class', 'list-group-item').attr('id', `${newLiFilterDelId}`);
-
+    // Append new 'li' element to 'ulTagEle', 'ulValueEle', and 'ulDelEle' with 'class' and 'id', respectively
+    let newLiTagEle = ulTagEle.append('li').attr('class', 'list-group-item').attr('id', `${newLiTagId}`);
+    let newLiValueEle = ulValueEle.append('li').attr('class', 'list-group-item').attr('id', `${newLiValueId}`);
+    let newLiDelEle = ulDelEle.append('li').attr('class', 'list-group-item').attr('id', `${newLiDelId}`);
     // Append new 'label' element to new 'li' in all forms, respectively
-    newLiTag.append('label').attr('for', `${newSelFilterId}`).text("Filter");
-    newLiValueTag.append('label').attr('for', `${newSelFilterValueId}`).text("Filter value");
-    newLiDel.append('label').attr('for', `${newBtnFilterDelId}`);
-
+    newLiTagEle.append('label').attr('for', `${newSelTagId}`).text("Filter");
+    newLiValueEle.append('label').attr('for', `${newSelValueId}`).text("Filter value");
+    newLiDelEle.append('label').attr('for', `${newBtnDelId}`);
     // Append new 'button' element to new 'li' with 'id', 'type', and 'class'
-    newLiDel.append('button').attr('id', `${newBtnFilterDelId}`).attr('type', 'submit').attr('class', 'btn btn-filter-del btn-outline-danger font-weight-bold').text("Delete Filter");
-
+    newLiDelEle.append('button').attr('id', `${newBtnDelId}`).attr('type', 'submit').attr('class', 'btn btn-filter-del btn-outline-danger font-weight-bold').text("Delete Filter");
     // Append new 'select' element to new 'li' with 'class' and 'id', respectively
-    let newSelTag = newLiTag.append('select').attr('class', 'sel-filter-tag').attr('id', `${newSelFilterId}`);
-    let newSelValueTag = newLiValueTag.append('select').attr('class', 'sel-filter-value').attr('id', `${newSelFilterValueId}`);
-
+    let newSelTagEle = newLiTagEle.append('select').attr('class', 'sel-filter-tag').attr('id', `${newSelTagId}`);
+    let newSelValueEle = newLiValueEle.append('select').attr('class', 'sel-filter-value').attr('id', `${newSelValueId}`);
     // Append new 'option' element to new 'select' in the first two forms and set as placeholder for dropdown menu in both forms, respectively
-    newSelTag.append('option').attr('value', 'placeholder').attr('selected', true).attr('disabled', true).attr('hidden', true).text("Select a filter");
-    newSelValueTag.append('option').attr('value', 'placeholder').attr('selected', true).attr('disabled', true).attr('hidden', true).text("Choose a filter from left");
+    newSelTagEle.append('option').attr('value', 'placeholder').attr('selected', true).attr('disabled', true).attr('hidden', true).text("Select a filter");
+    newSelValueEle.append('option').attr('value', 'placeholder').attr('selected', true).attr('disabled', true).attr('hidden', true).text("Choose a filter from left");
     
     // Check if there are more than 5 filters
-    if (maxLayer<5) {
+    // Note that "currAvailLayerArr" is updated by ".shift()"
+    if (currAvailLayerArr.length !== 0) {
         // Enable "Add Filter" button if true
-        addFilter.attr('disabled', null);
+        butAddEle.attr('disabled', null);
         // Disable "Add Filter" button if false
     } else {
-        addFilter.attr('disabled', true);
+        butAddEle.attr('disabled', true);
     }
-    // CAN BE FOUND IF SELECT FROM THE SAME EVENT LISTENER
-    console.log(`'Add Filter' event: there are ${maxLayer} layers of filters`)
-    console.log(`'Add Filter' event: d3.selection has identified ${d3.selectAll('.sel-filter-tag').size()} targeted 'select' element.`);
+
+    // VERY IMPORTANT: EVENT LISTENERS NEED TO BE UPDATED TO GRAP NEWLY APPENDED 'SELECT' ELEMENTS ONCE NEW FILTER IS ADDED
+    // Note that passing d3.selectAll('.sel-filter-tag') to globally defined variable does not work
     formDisplayAvailFilters();
     formSelectFilterName();
     formSelectFilterValue();
@@ -104,7 +102,7 @@ addFilter.on("click", function() {
 // >>>>>>>>>>>>>>>       START      >>>>>>>>>>>>>>>
 
 // Event listener for 'Delete Filter' button
-d3.select('.btn-filter-del').on("click", function() {
+d3.selectAll('.btn-filter-del').on("click", function() {
    
     if (maxLayer === 1) {
 
@@ -116,13 +114,13 @@ d3.select('.btn-filter-del').on("click", function() {
         d3.event.preventDefault();
 
         // Retrieve active layer
-        let activeLayer = parseInt(this.id.split('-')[3]);
+        let activeLayer = this.id.split('-')[3];
         console.log(activeLayer)
 
         // Generate 'id' names for 'li' elements in the same layer
-        let paralLiFilterId = basicLiTag + activeLayer.toString();
-        let paralLiFilterValueId = basicLiValue + activeLayer.toString();
-        let activeLiFilterDelId = basicLiDel + activeLayer.toString();   
+        let paralLiFilterId = basicLiTag + activeLayer;
+        let paralLiFilterValueId = basicLiValue + activeLayer;
+        let activeLiFilterDelId = basicLiDel + activeLayer;   
 
         // Remove 'li' elements
         d3.select(`#${paralLiFilterId}`).remove();    
@@ -133,8 +131,8 @@ d3.select('.btn-filter-del').on("click", function() {
         usedFilterInfo = usedFilterInfo.filter((d) => !Object.keys.includes(d));
 
         // Enable "Add Filter" button if there is data in filtered UFO dataset
-        if (availFilteredDataset() !== []) {
-            addFilter.attr('disabled', null);
+        if (availFilteredUfoDataset() !== []) {
+            butAddEle.attr('disabled', null);
         }
     }
 
@@ -145,7 +143,7 @@ d3.select('.btn-filter-del').on("click", function() {
 // >>>>>>>>>>>>>>>       START      >>>>>>>>>>>>>>>
 
 // Event listener for "Submit" button
-submit.on('click', function() {
+d3.select('#filter-submit').on('click', function() {
 
     // Prevent the webpage from self-refreshing
     d3.event.preventDefault();
@@ -157,7 +155,7 @@ submit.on('click', function() {
     tbody.html('');
 
     // Determine currently available filtered UFO dataset
-    let currAvailUfoDataset = availFilteredDataset();
+    let currAvailUfoDataset = availFilteredUfoDataset();
 
     // Display filtered data in table
     currAvailUfoDataset.forEach((d) => {
@@ -197,18 +195,18 @@ function uniqueFilterValue(myKey, myArrayOfObjects) {
 /**
  * Apply filters from "usedFilterInfo" to "ufoDataset" and return currently available ufo dataset
  */
-function availFilteredDataset() {
+function availFilteredUfoDataset() {
 
     // Retrieve array containing each unavailable (applied) filter tags w/o values
-    let usedFilterInfoArray = Object.values(usedFilterInfo);
+    let usedFilterArr = Object.values(usedFilterInfo);
     // Narrow down array to contain each available filter tags and values
-    let usedFilterInfoArrayWithValue = usedFilterInfoArray.filter((ele) => ele.length === 2);
+    let usedFilterNameAndValueArr = usedFilterArr.filter((ele) => ele.length === 2);
     // Set initial current available ufo dataset array as "ufoDataset"
     let currAvailUfoDataset = ufoDataset;
     // Retrieve array containing currently availalbe ufo dataset
-    if (usedFilterInfoArrayWithValue.length > 0) {
-        for (let i=0; i<usedFilterInfoArrayWithValue.length; i++) {
-            currAvailUfoDataset = currAvailUfoDataset.filter((d) => d[usedFilterInfoArrayWithValue[i][0]] === usedFilterInfoArrayWithValue[i][1]);
+    if (usedFilterNameAndValueArr.length > 0) {
+        for (let i=0; i<usedFilterNameAndValueArr.length; i++) {
+            currAvailUfoDataset = currAvailUfoDataset.filter((d) => d[usedFilterNameAndValueArr[i][0]] === usedFilterNameAndValueArr[i][1]);
         }
     }
     // Return array containing currently availalbe ufo dataset as callback value
@@ -221,9 +219,9 @@ function availFilteredDataset() {
  */
 function getCurrAvailFilter() {
     // Retrieve array containing each unavailable (applied) filter tags w/o values
-    let usedFilterInfoArray = Object.values(usedFilterInfo);    
+    let usedFilterArr = Object.values(usedFilterInfo);    
     // Retrieve array containing currently applied filter tags
-    let currUnavailFilter = usedFilterInfoArray.map((ele) => ele[0]);
+    let currUnavailFilter = usedFilterArr.map((ele) => ele[0]);
     // Determine currently available filter tags
     let currAvailFilter = ufoFilterArr.filter((ele) => !currUnavailFilter.includes(ele));
     // Return array containing currently available filters as callback value
@@ -238,17 +236,17 @@ function getCurrAvailFilter() {
  * Display array of available filters as dropdown list in "filter name" form
  */
 function formDisplayAvailFilters() {
-    
+ 
     // Event listener ("mousedown") on active 'select' element for filter name
     d3.selectAll('.sel-filter-tag').on("mousedown", function() {     
-
+ 
         // . ............... Event validation and info retrival ............... .//
         // Retrieve form layer of fired event
-        let eventLayer = parseInt(this.id.split('-')[3]);
+        let eventLayer = this.id.split('-')[3];
         // Retrieve form text of fired event
         let eventText = this.value;
         // Validate firing of "mousedown" event
-        console.log(`Event "mousedown" fired: text of "${eventText}" and layer #${eventLayer} in the form are retrieved.`);
+        console.log(`Event "mousedown" fired: text of "${eventText}" and "${eventLayer}" layer in the form are retrieved.`);
 
         // . ............... Application of retrival info ............... .//
         // Update object of used filter to remove data for current form layer if exists
@@ -258,23 +256,29 @@ function formDisplayAvailFilters() {
         
         // Determine array of available filters
         let availFilterArr = getCurrAvailFilter();
-        // Add pre-existing filter name to array of available filters because we want it also to be selectable
-        if (eventText !== 'placeholder') {
-            availFilterArr.push(eventText);
-        }
+        // // Add pre-existing filter name to array of available filters because we want it also to be selectable
+        // if (eventText !== 'placeholder') {
+        //     availFilterArr.push(eventText);
+        // }
 
         // Grab 'select' element for current form layer
-        let eventSelTagId = basicSelTag + eventLayer.toString();
+        let eventSelTagId = basicSelTag + eventLayer;
         let eventSelTagEle = d3.select(`#${eventSelTagId}`);
 
         // Remove all pre-existing 'option' element
         eventSelTagEle.selectAll('option').remove();
-        // Append available filters to 'option' element for display in dropdown menu
-        // Note that there is no need to put back placeholder 'option' element
-        availFilterArr.forEach((filter) => {
-            eventSelTagEle.append('option').attr('value', filter).text(filter);
 
-        });      
+        // Put back pre-existing displayed 'option' element because we want it also to be selectable
+        if (eventText !== 'placeholder') {
+            eventSelTagEle.append('option').attr('value', eventText).text(eventText);
+        }
+        // Append available filters to 'option' element for display in dropdown menu
+        availFilterArr.forEach((filter) => {
+            eventSelTagEle.append('option').attr('value', filter).text(filter);             
+        });
+        // THE APPENDING ORDER IS VERY IMPORTANT. PRE-EXISTING FILTER SHOULD BE APPENDED FIRSTLY AND INDIVIDUALLY
+        // PUTTING IT BACK TO AVAILABLE FILTER ARRAY TO BE APPENDED TOGETHER WILL OTHERWISE RENDER A SWITCH BY SHOWING THE FIRST FILTER NAME IN ALPHABETICAL ORDER, WHICH MESSED UP THE ENTIRE LIST
+
     });
 }
 
@@ -292,12 +296,12 @@ function formSelectFilterName() {
 
         // . ............... Event validation and info retrival ............... .//
         // Retrieve form layer of fired event
-        let eventLayer = parseInt(this.id.split('-')[3]);
+        let eventLayer = this.id.split('-')[3];
         // Retrieve form text of fired event
         let eventText = this.value;
         // Validate firing of "change" event
-        console.log(`Event (tag) "change" fired: filter name of "${eventText}" and layer #${eventLayer} in the form are retrieved.
-                    d3.selection has identified ${d3.selectAll('.sel-filter-tag').size()} qualified 'select' elements.`);
+        console.log(`Event (tag) "change" fired: filter name of "${eventText}" and "${eventLayer}" layer in the form are retrieved.
+                    d3.selection has identified ${d3.selectAll('.sel-filter-tag').size()} qualified 'select' ele.`);
         
         // . ............... Application of retrival info ............... .//
         // Update object of used filter with current form layer as well as name of applied filter
@@ -305,13 +309,13 @@ function formSelectFilterName() {
         usedFilterInfo[eventLayer] = [eventText];
 
         // Determine array of unique values for selected filter
-        let availUniqueFilterValueArr = uniqueFilterValue(eventText, availFilteredDataset());
+        let availUniqueFilterValueArr = uniqueFilterValue(eventText, availFilteredUfoDataset());
 
         // Grab 'select' element on the same layer but from "filter value" form
-        let eventSelValueId = basicSelValue + eventLayer.toString();
+        let eventSelValueId = basicSelValue + eventLayer;
         let eventSelValueEle = d3.select(`#${eventSelValueId}`);
         // Determine 'class' name for 'option' element added by javascript within the above 'select' element        
-        let eventOptValueClass = basicOptValue + eventLayer.toString();
+        let eventOptValueClass = basicOptValue + eventLayer;
         // Remove all pre-existing javascript-added 'option' element if any
         // Note that placeholder 'option' element is not removed
         eventSelValueEle.selectAll(`.${eventOptValueClass}`).remove();
@@ -341,12 +345,12 @@ function formSelectFilterValue() {
 
         // . ............... Event validation and info retrival ............... .//
         // Retrieve form layer of fired event
-        let eventLayer = parseInt(this.id.split('-')[3]);
+        let eventLayer = this.id.split('-')[3];
         // Retrieve form text of fired event
         let eventText = this.value;
         // Validate firing of "change" event
-        console.log(`Event (value) "change" fired: filter value of "${eventText}" and layer #${eventLayer} in the form are retrieved.
-                    d3.selection has identified ${d3.selectAll('.sel-filter-tag').size()} qualified 'select' elements.`);
+        console.log(`Event (value) "change" fired: filter value of "${eventText}" and "${eventLayer}" layer in the form are retrieved.
+                    d3.selection has identified ${d3.selectAll('.sel-filter-tag').size()} qualified 'select' ele.`);
 
         // . ............... Application of retrival info ............... .//
         // Update object of used filter with filter value for current form layer
@@ -360,14 +364,14 @@ function formSelectFilterValue() {
         }
         
         // Disable "Add Filter" button if selecting current filter value results in only one filtered UFO dataset
-        if (maxLayer !== 5) {
+        if (Object.keys(usedFilterInfo).length !== 5) {
             // Only execute the following codes when not all filters are applied in filtering UFO dataset
-            if (availFilteredDataset().length === 1) {
+            if (availFilteredUfoDataset().length === 1) {
                 // Disable "Add Filter" button if true
-                addFilter.attr('disabled', true);
+                butAddEle.attr('disabled', true);
             } else {
                 // Enable "Add Filter" button if false
-                addFilter.attr('disabled', null);
+                butAddEle.attr('disabled', null);
             }
         }
 
